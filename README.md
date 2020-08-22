35aafe1ef38bbc4fca485de613685373df4fa315
Michael C. Albers <michael.c.albers@gmail.com>
2020-08-22 16:08:10 -0700

    great update. rules firing.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Clone & Extend
 
 This repository is a clone of
-[Sketch's assistant template](https://github.com/sketch-hq/sketch-assistant-template/) and extends
+[Sketch's assistant template](https://github.com/sketch-hq/sketch-assistant-template/) and steals
+from and extends
 [Sketch's Naming Conventions assistant](https://github.com/sketch-hq/sketch-assistants/tree/main/assistants/naming-conventions).
 Well, I hope it does; this is still an experiment.
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
-import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+import { AssistantPackage } from '@sketch-hq/sketch-assistant-types'
+import CoreAssistant from '@sketch-hq/sketch-core-assistant'
 
-const helloNC: RuleDefinition = {
-  rule: async (context) => {
-    context.utils.report('naming conventions here...')
-  },
-  name: 'naming-conventions-assistant-plus/hello-naming-conventions',
-  title: 'Naming Conventions Plus',
-  description: 'Reports a hello world message',
-}
-
-const assistant: AssistantPackage = async () => {
-  return {
-    name: 'naming-conventions-assistant-plus',
-    rules: [helloNC],
-    config: {
-      rules: {
-        'naming-conventions-assistant-plus/hello-naming-conventions': { active: true },
+const assistant: AssistantPackage = [
+  CoreAssistant,
+  async () => {
+    return {
+      name: '@michaelcalbers/naming-conventions-assistant-plus',
+      rules: [],
+      config: {
+        rules: {
+          '@sketch-hq/sketch-core-assistant/name-pattern-shapes': {
+            active: true,
+            allowed: [],
+            forbidden: ['^Rectangle$', 'Rectangle Copy'],
+            ruleTitle: 'Rectangle names should not be default',
+          },
+          '@sketch-hq/sketch-core-assistant/name-pattern-groups': {
+            active: true,
+            allowed: [],
+            forbidden: ['^Group$', 'Group Copy'],
+            ruleTitle: 'Group names should not be default',
+          },
+          '@sketch-hq/sketch-core-assistant/name-pattern-symbols': {
+            active: true,
+            allowed: ['(^.+)(([/].*)+)\\w$'],
+            forbidden: [],
+            ruleTitle: 'Symbol names should use forward slash grouping',
+          },
+        },
       },
-    },
-  }
-}
+    }
+  },
+]
 
 export default assistant
