09c3d1c8d6a134647244f4f23a5a0192587b5d33
Michael C. Albers <michael.c.albers@gmail.com>
2020-08-23 14:34:22 -0700

    good Beta
    flags default names for:
    * artboards
    * groups
    * all shapes
    flags synbols not constructed using a /

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,42 @@ const assistant: AssistantPackage = [
       rules: [],
       config: {
         rules: {
-          '@sketch-hq/sketch-core-assistant/name-pattern-shapes': {
+          '@sketch-hq/sketch-core-assistant/name-pattern-artboards': {
             active: true,
             allowed: [],
-            forbidden: ['^Rectangle$', 'Rectangle Copy'],
-            ruleTitle: 'Rectangle names should not be default',
+            forbidden: ['^Artboard$', 'Artboard Copy', 'Artboard [0-9]'],
+            ruleTitle: 'Give Artboards informative descriptions (vs. their default names)',
           },
           '@sketch-hq/sketch-core-assistant/name-pattern-groups': {
             active: true,
             allowed: [],
-            forbidden: ['^Group$', 'Group Copy'],
-            ruleTitle: 'Group names should not be default',
+            forbidden: ['^Group$', 'Group Copy', 'Group [0-9]'],
+            ruleTitle: 'Give Groups informative descriptions (vs. their default names)',
+          },
+          '@sketch-hq/sketch-core-assistant/name-pattern-shapes': {
+            active: true,
+            allowed: [],
+            forbidden: [
+              '^Rectangle$',
+              'Rectangle Copy',
+              'Rectangle [0-9]',
+              '^Oval$',
+              'Oval Copy',
+              'Oval [0-9]',
+              '^Line$',
+              'Line Copy',
+              'Line [0-9]',
+              '^Triangle$',
+              'Triangle Copy',
+              'Triangle [0-9]',
+              '^Star$',
+              'Star Copy',
+              'Star [0-9]',
+              '^Polygon$',
+              'Polygon Copy',
+              'Polygon [0-9]',
+            ],
+            ruleTitle: 'Give Shapes informative descriptions (vs. their default names)',
           },
           '@sketch-hq/sketch-core-assistant/name-pattern-symbols': {
             active: true,
